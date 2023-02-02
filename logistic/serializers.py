b79d4521bbb77e89fb335c66476afa3f15011371
Quzi <quzi.steam@ya.ru>
2023-02-02 17:05:47 +0300

    Initial commit

---
+++ logistic/serializers.py
@@ -0,0 +1,53 @@
+from rest_framework import serializers
+
+from logistic.models import Product, Stock, StockProduct
+
+
+class ProductSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Product
+        fields = '__all__'
+
+
+class ProductPositionSerializer(serializers.ModelSerializer):
+    # настройте сериализатор для позиции продукта на складе
+    class Meta:
+        model = StockProduct
+        fields = ['product', 'quantity', 'price']
+    
+
+class StockSerializer(serializers.ModelSerializer):
+    positions = ProductPositionSerializer(many=True)
+
+    class Meta:
+        model = Stock
+        fields = ['address', 'positions', ]
+
+    def create(self, validated_data):
+        # достаем связанные данные для других таблиц
+        positions = validated_data.pop('positions')
+
+        # создаем склад по его параметрам
+        stock = super().create(validated_data)
+        for position in positions:
+            StockProduct(stock_id=stock.id, product=position['product'],
+                         price=position['price'], quantity=position['quantity']).save()
+        # здесь вам надо заполнить связанные таблицы
+        # в нашем случае: таблицу StockProduct
+        # с помощью списка positions
+
+        return stock
+
+    def update(self, instance, validated_data):
+        # достаем связанные данные для других таблиц
+        positions = validated_data.pop('positions')
+
+        # обновляем склад по его параметрам
+        stock = super().update(instance, validated_data)
+        for position in positions:
+            StockProduct.objects.update_or_create(stock=stock, product=position.get('product'),)
+        # здесь вам надо обновить связанные таблицы
+        # в нашем случае: таблицу StockProduct
+        # с помощью списка positions
+
+        return stock
