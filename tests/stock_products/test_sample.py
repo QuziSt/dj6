a571d3d90fcb8919e5d742a1050c99dbcaed781a
Quzi <quzi.steam@ya.ru>
2023-03-19 15:11:41 +0300

    changed8

--- tests/stock_products/test_sample.py
+++ tests/stock_products/test_sample.py
@@ -1,9 +1,5 @@
 import pytest
 from rest_framework.test import APIClient
-import os
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mysite.settings')
-import django
-django.setup()
 
 def test_sample():
     client = APIClient()
